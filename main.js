d50053eed31b3de35d38ba5245faf0b29314757d
Kangseohyeon <kshyeon123@gmail.com>
2023-02-17 20:32:20 +0900

    Connecting Electron

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, session } = require("electron");
+const isDev = require("electron-is-dev");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 300,
-    height: 300,
+    width: 800,
+    height: 800,
+    title: "Pixel Art Drawer",
   });
 
   win.loadURL("http://127.0.0.1:3000");
+
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
   return win;
 };
 
 app.whenReady().then(() => {
   win = createWindow();
+  // session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+  //   callback({
+  //     responseHeaders: {
+  //       ...details.responseHeaders,
+  //       "Content-Security-Policy": ["default-src 'none'"],
+  //     },
+  //   });
+  // });
 });
 
 app.on("window-all-closed", () => {
@@ -22,7 +36,7 @@ app.on("window-all-closed", () => {
 
 // when application is activated, open default window
 app.on("activate", () => {
-  // if (BrowserWindow.getAllWindows().length === 0) {
-  //   createWindow(); // open default window
-  // }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow(); // open default window
+  }
 });
